3effd8599e1957702777fb583e90cf90414f3a8a
Igor Kreslo <Igor.Kreslo@cern.ch>
2017-11-23 14:36:28 +0100

    Added pictures for README

---
+++ scripts/Append.C
@@ -0,0 +1,30 @@
+CRTRawhit *hit;
+CRTEvent *evt;
+CRT2Dhit *e2d;
+CRTRawhitBuffer *rhbuf;
+CRTCalibs*  cal;
+CRTRun * run;
+CRTRun * run1;
+CRTRun * run2;
+
+
+void  Append(const char *fname1="",const char *ofname="")
+{
+  run=new CRTRun();
+  run1=new CRTRun();
+  if(strlen(fname1)==0) {printf("run1: No input file name is given.\n");return;};
+  if(strlen(ofname)==0) {printf("No output file name is given.\n");return;};
+  if(run1->OpenExistingDataRun(fname1)==0){printf("run1: Extracting CRTRun object is failed.\n");return;};
+
+  if(run->OpenExistingDataRun(ofname, "UPDATE")==0) {printf("Output run does not exist. Creating new one.\n"); run->CreateNewDataRun(ofname);}
+  else {printf("Opened run for update: \n"); run->PrintSummary();}
+  run->AppendSortedByTime(run1);
+  run->BuildIndex("h2d.s","int(h2d.t0)");
+  run->PrintSummary();
+
+  run->Close();
+}
+
+
+
+
---
+++ scripts/TPCMatch.C
@@ -0,0 +1,25 @@
+CRTRawhit *hit;
+CRTEvent *evt;
+CRT2Dhit *e2d;
+CRTRawhitBuffer *rhbuf;
+CRTCalibs*  cal;
+CRTRun * run;
+CRTRun * run1;
+CRTRun * run2;
+CRTReco *reco;
+
+
+void TPCMatch(const char *fncrt="",const char *fntpc="",const char *ofname="")
+{
+  reco=new CRTReco();
+  run=new CRTRun();
+  if(strlen(fncrt)==0) {printf("CRT data: No input file name is given.\n");return;};
+  if(strlen(fntpc)==0) {printf("TPC data: No input file name is given.\n");return;};
+  if(strlen(ofname)==0) {printf("No output file name is given.\n");return;};
+  reco->MatchFlashestoCRT(const char * fncrt, const char *fntpc, const char *ofname);
+  
+}
+
+
+
+
